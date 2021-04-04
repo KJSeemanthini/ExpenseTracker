ef7f46606464a14582240cb1ff7ee22227dba061
KJSeemanthini <seema.96@gmail.com>
2021-04-04 14:57:06 +0530

    same functionality with onsubmit event

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -7,7 +7,18 @@ const AddTransaction = () => {
   const { dispatch } = useContext(StateContext);
   const setNameEvent = (e) => setName(e.target.value);
 
-  const addTransaction = () => {
+  // const addTransaction = () => {
+  //   dispatch({
+  //     type: "AddTransaction",
+  //     item: {
+  //       id: Math.floor(Math.random() * 1000000),
+  //       name: name,
+  //       expense: +amount
+  //     }
+  //   });
+  // };
+  const onSubmit = (e) => {
+    e.preventDefault();
     dispatch({
       type: "AddTransaction",
       item: {
@@ -21,19 +32,31 @@ const AddTransaction = () => {
   return (
     <div className="addTransaction">
       <h2>Add new Transaction</h2>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setNameEvent(e)}
-        placeholder="Expense Name"
-      />
-      <input
-        type="number"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-        placeholder="Amount"
-      />
-      <button onClick={addTransaction}>Add Transaction</button>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>Text</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setNameEvent(e)}
+            placeholder="Expense Name"
+          />
+        </div>
+        <div>
+          <label>Amount</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Amount"
+          />
+        </div>
+        {/* <button onClick={addTransaction}>Add Transaction</button> */}
+        <div>
+          {/* <label> Submit</label> */}
+          <input type="Submit" value="submit" />
+        </div>
+      </form>
     </div>
   );
 };
